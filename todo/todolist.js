38f8845e9389e2a408d4f5f37f637f70050f0ddf
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-03-30 16:07:18 -0400

    Added first 4 methods to Todo class

--- todo/todolist.js
+++ todo/todolist.js
@@ -32,3 +32,56 @@ class Todo {
     return this.title;
   }
 }
+
+
+// This class represents a collection of Todo objects.
+// You can perform typical collection-oriented actions
+// on a TodoList object, including iteration and selection.
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(todo) {
+    if (!(todo instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
+    this.todos.push(todo);
+  }
+  size() {
+    return this.todos.length;
+  }
+  first() {
+    return this.todos[0];
+  }
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  // rest of class needs implementation
+}
+
+let list = new TodoList("Today's Todos");
+console.log(list); // TodoList { title: "Today's Todos", todos: [] }
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+console.log(list);
+
+console.log(list.size());  // 4
+
+console.log(list.first());
+console.log(list.last());
+
+let emptyList = new TodoList("Empty List");
+console.log(emptyList.first());
+console.log(emptyList.last());
