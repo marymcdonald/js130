2933f8e76ebc30793680c88ce1af6f162ff0c697
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-03-31 14:59:46 -0400

    Finished first assignment

--- todo/todolist.js
+++ todo/todolist.js
@@ -59,29 +59,48 @@ class TodoList {
   last() {
     return this.todos[this.size() - 1];
   }
+  //my solution:
+  // itemAt(index) {
+  //   if (!(this.todos[index])) {
+  //     throw new ReferenceError(`invalid index: ${index}`);
+  //   }
+  //   return this.todos[index];
+  // }
 
-  // rest of class needs implementation
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) { // _ in name suggests a "private" method
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+  isDone() {
+    return this.todos.every(element => element.isDone() === true);
+  }
+  shift() {
+    return this.todos.shift();
+  }
+  pop() {
+    return this.todos.pop();
+  }
+  removeAt(index) {
+    let removedItem = this.itemAt(index);
+    this.todos.splice(index, 1);
+    return removedItem;
+  }
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map(todo => todo.toString()).join('\n');
+    return `${title}\n${list}`;
+  }
 }
-
-let list = new TodoList("Today's Todos");
-console.log(list); // TodoList { title: "Today's Todos", todos: [] }
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-console.log(list);
-
-console.log(list.size());  // 4
-
-console.log(list.first());
-console.log(list.last());
-
-let emptyList = new TodoList("Empty List");
-console.log(emptyList.first());
-console.log(emptyList.last());
